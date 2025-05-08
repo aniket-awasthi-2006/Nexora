d4e9606957d5837129b0e5bc792a4fccf64a7ef7
Aniket Awasthi <paradoxplayz95@gmail.com>
2025-05-08 08:25:19 +0530

    Dashboard page modified 101

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -20,23 +20,75 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     final items = <Widget>[
-      Padding(padding:EdgeInsets.all(8),child:Icon(CupertinoIcons.collections_solid,size:35,color: Colors.white)),
-      Padding(padding:EdgeInsets.all(8),child:Icon(CupertinoIcons.sparkles,size:35,color: Colors.white)),
-      Padding(padding:EdgeInsets.all(8),child:Icon(Icons.leaderboard_rounded,size:35,color: Colors.white)),
-
+      Padding(
+          padding: EdgeInsets.all(10),
+          child: Icon(CupertinoIcons.collections_solid,
+              size: 25, color: Colors.white)),
+      Padding(
+          padding: EdgeInsets.all(10),
+          child:
+              Icon(CupertinoIcons.sparkles, size: 25, color: Colors.white)),
+      Padding(
+          padding: EdgeInsets.all(10),
+          child: Icon(Icons.leaderboard_rounded, size: 25, color: Colors.white)),
     ];
 
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 30, 30, 30),
-      bottomNavigationBar:CurvedNavigationBar(
-        height: 75 ,
-        color: const Color.fromARGB(255, 20, 20, 20),
-        items: items,
-        backgroundColor: Colors.transparent,
+      backgroundColor: const Color.fromARGB(255, 28, 28, 28),
+      body:SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 80, 0, 0),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(begin: Alignment.topCenter,
+                  end:Alignment.center,
+                  colors: [Color.fromARGB(100, 0, 0, 0),
+                           Color.fromARGB(0, 0, 0, 0)],
+                ),
+                borderRadius: BorderRadius.circular(20)),
+              child: SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(5, 40, 5, 75), // bottom padding for overlap
+                child: Column(
+                  children: List.generate(
+                    50,
+                    (index) => ListTile(
+                      title: Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: LinearGradient(
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight,
+                                colors: <Color>[
+                                  Color.fromARGB(255, 0, 0, 0),
+                                  Color.fromARGB(255, 20, 20, 20),
+                                  Color.fromARGB(255, 40, 40, 40),
+
+                                ])),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child:CurvedNavigationBar(
+                  height: 75,
+                  color: const Color.fromARGB(255, 23, 23, 23),
+                  items: items,
+                  backgroundColor: Colors.transparent,
+                ),
+              ),
+          ],
+        ),
         
-        
-        ), );
+      ),
+    
+    );
   }
-}
+  }
